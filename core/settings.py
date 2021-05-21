6c917f183b5e693d649f114418b7c7243bfbc787
Vadym Mariiechko <vadimich348@gmail.com>
2021-05-21 18:36:31 +0200

    Fix static root url

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -113,6 +114,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 STATIC_URL = '/static/'
 
